47c025332736ab21b58431856f73d3e1edcc6ba7
Felipe Lima Neves <167237385+Pylipe@users.noreply.github.com>
2025-07-30 23:18:11 +0000

    Definição de Variáveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,8 +5,11 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
+  printf("Desafio Super Trunfo!\n");
   // Área para definição das variáveis para armazenar as propriedades das cidades
-  int
+    int População, Pontos;
+    char Estado, Códigos, Cidade;
+    float Area, PIB;
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
