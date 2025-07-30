1e5688ed7cd31eef87c686efdf254f2ba4a515bb
Felipe Lima Neves <167237385+Pylipe@users.noreply.github.com>
2025-07-30 23:03:24 +0000

    Criei o prnt f

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,7 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+  int
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
