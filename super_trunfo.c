3d994a4a1b78fdf7e8b84e774e640955733a1697
Felipe Lima Neves <167237385+Pylipe@users.noreply.github.com>
2025-08-02 00:51:00 +0000

    declaração de variáveis da carta 1

--- super_trunfo.c
+++ super_trunfo.c
@@ -4,7 +4,7 @@ int main() {
     // Cabeçalho do jogo
     printf("===== Cadastro de Cartas - Super Trunfo de Países =====\n\n");
 
-    // Declaração das variáveis da Carta 1
+     // Declaração das variáveis da Carta 1
     char estado1;
     char codigo1[5];
     char cidade1[30];
@@ -36,58 +36,3 @@ int main() {
     scanf("%d", &pontos1);
 
     printf("\n");
-
-    // Declaração das variáveis da Carta 2
-    char estado2;
-    char codigo2[5];
-    char cidade2[30];
-    int populacao2, pontos2;
-    float area2, pib2;
-
-    // Entrada de dados da Carta 2
-    printf("Cadastro da Carta 2:\n");
-    printf("Digite o Estado (A a H): ");
-    scanf(" %c", &estado2);
-
-    printf("Digite o Código da Carta (ex: B03): ");
-    scanf("%s", codigo2);
-
-    printf("Digite o Nome da Cidade: ");
-    getchar(); // limpa o buffer
-    scanf("%[^\n]", cidade2);
-
-    printf("Digite a População: ");
-    scanf("%d", &populacao2);
-
-    printf("Digite a Área (em km²): ");
-    scanf("%f", &area2);
-
-    printf("Digite o PIB (em bilhões de reais): ");
-    scanf("%f", &pib2);
-
-    printf("Digite o Número de Pontos Turísticos: ");
-    scanf("%d", &pontos2);
-
-    // Exibição das informações das cartas
-    printf("\n===== Cartas Cadastradas =====\n\n");
-
-    printf("Carta 1:\n");
-    printf("Estado: %c\n", estado1);
-    printf("Código: %s\n", codigo1);
-    printf("Nome da Cidade: %s\n", cidade1);
-    printf("População: %d\n", populacao1);
-    printf("Área: %.2f km²\n", area1);
-    printf("PIB: %.2f bilhões de reais\n", pib1);
-    printf("Número de Pontos Turísticos: %d\n", pontos1);
-
-    printf("\nCarta 2:\n");
-    printf("Estado: %c\n", estado2);
-    printf("Código: %s\n", codigo2);
-    printf("Nome da Cidade: %s\n", cidade2);
-    printf("População: %d\n", populacao2);
-    printf("Área: %.2f km²\n", area2);
-    printf("PIB: %.2f bilhões de reais\n", pib2);
-    printf("Número de Pontos Turísticos: %d\n", pontos2);
-
-    return 0;
-}
