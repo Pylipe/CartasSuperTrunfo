849b60e42d52181fa7676810656c6b7446fd9ff5
Felipe Lima Neves <167237385+Pylipe@users.noreply.github.com>
2025-08-09 02:12:27 +0000

    entrada de dados da carta 1

--- super_trunfo.c
+++ super_trunfo.c
@@ -2,11 +2,38 @@
 
     int main() {
     //Cabeçalho
-    printf (*** Cadastro de cartas - Super Trunfo ***);
+    printf ("***Cadastro de cartas - Super Trunfo***\n");
 
     //Declaração de variáveis
     char estado1, estado2, codigo1[3], codigo2[3], cidade1, cidade2;
-    int populacao1, populacao2; pontos1, pontos2;
+    int populacao1, populacao2, pontos1, pontos2;
+    float pib1, pib2, area1, area2;
+
+    //Solicitação de dados
+    // Entrada de dados da Carta 1
+    printf("Cadastro da Carta 1:\n");
+    printf("Digite o Estado (A a H): ");
+    scanf(" %c", &estado1);
+
+    printf("Digite o Código da Carta (ex: A01): ");
+    scanf("%s", codigo1);
+
+    printf("Digite o Nome da Cidade: ");
+    scanf("%s", cidade1);
+
+    printf("Digite a População: ");
+    scanf("%d", &populacao1);
+
+    printf("Digite a Área (em km²): ");
+    scanf("%f", &area1);
+
+    printf("Digite o PIB (em bilhões de reais): ");
+    scanf("%f", &pib1);
+
+    printf("Digite o Número de Pontos Turísticos: ");
+    scanf("%d", &pontos1);
+
+    printf("\n");
     
 
 
