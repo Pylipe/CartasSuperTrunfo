ed2ce0232126f9f678e8e0eccfb1ab1db175ea8f
Felipe Lima Neves <167237385+Pylipe@users.noreply.github.com>
2025-08-09 01:45:30 +0000

    declaração de variáveis

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
     int main() {
-    //
+    //Cabeçalho
+    printf (*** Cadastro de cartas - Super Trunfo ***);
+
+    //Declaração de variáveis
+    char estado1, estado2, codigo1[3], codigo2[3], cidade1, cidade2;
+    int populacao1, populacao2; pontos1, pontos2;
+    
+
+
+
 
 
 
